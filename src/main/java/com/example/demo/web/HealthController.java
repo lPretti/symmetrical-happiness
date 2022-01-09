79fb12aa4865720a48e581dd3bf57d0cd4b16012
Isabellin Pripst <lPretti>
2022-01-08 23:33:50 -0300

    add: web folder

---
+++ src/main/java/com/example/demo/web/HealthController.java
@@ -0,0 +1,12 @@
+package com.example.demo.web;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HealthController {
+    @GetMapping("/health")
+    public String health(){
+        return "I'm alive";
+    }
+}
---
+++ src/main/java/com/example/demo/web/TableController.java
@@ -0,0 +1,30 @@
+package com.example.demo.web;
+
+import com.example.demo.domain.table.TableModel;
+import com.example.demo.domain.table.TableService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+@Controller
+public class TableController {
+
+    private TableService service;
+
+    public TableController(TableService service) {
+        this.service = service;
+    }
+
+    @GetMapping("/tables")
+    public ResponseEntity<List<TableModel>> getAllAvailable(){
+        return ResponseEntity.ok(service.getAllAvailable());
+    }
+
+    @GetMapping("/tables")
+    public ResponseEntity<TableModel> getTableById(@PathVariable("tableId")int tableId){
+        return  ResponseEntity.ok(service.getById(tableId));
+    }
+}
