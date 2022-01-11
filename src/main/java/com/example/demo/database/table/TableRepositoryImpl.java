13484f6aa530c5398dbf9b10fa5765c8e706e422
lPretti <lprettimolina@gmail.com>
2022-01-10 21:48:07 -0300

    TableRepository getByStatus

--- src/main/java/com/example/demo/database/table/TableRepositoryImpl.java
+++ src/main/java/com/example/demo/database/table/TableRepositoryImpl.java
@@ -7,10 +7,36 @@ import org.springframework.stereotype.Repository;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.stream.Collectors.toList;
+
 @Repository
 public class TableRepositoryImpl implements TableRepository {
+    private TableJpaRepository repository;
+
+    public TableRepositoryImpl(TableJpaRepository repository) {
+        this.repository = repository;
+    }
+
     @Override
-    public Optional<List<TableModel>> getAllAvailable() {
-        return Optional.empty();
+    public List<TableModel> getAllByTableStatus(int code) {
+        return repository.getAllByTableStatus(code)
+                .stream()
+                .map(this::toModel)
+                .collect(toList());
+    }
+
+    private TableModel toModel(TableEntity entity){
+        return new TableModel(entity.getId(),
+                entity.getStatus(),
+                entity.getSits(),
+                entity.isOutside());
+    }
+
+    private TableEntity toEntity(TableModel model){
+        return new TableEntity(model.getId(),
+                model.getStatus(),
+                model.getStatus().getCode(),
+                model.getSits(),
+                model.isOutside());
     }
 }
