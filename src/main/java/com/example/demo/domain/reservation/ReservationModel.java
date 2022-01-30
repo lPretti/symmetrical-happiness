b9323e5d19b463c3de1656935f0ab3cc7ae5be82
lPretti <lprettimolina@gmail.com>
2022-01-29 21:37:18 -0300

    shift actualization

--- src/main/java/com/example/demo/domain/reservation/ReservationModel.java
+++ src/main/java/com/example/demo/domain/reservation/ReservationModel.java
@@ -1,14 +1,13 @@
 package com.example.demo.domain.reservation;
 
 import com.example.demo.domain.customer.CustomerModel;
-import com.example.demo.domain.table.TableModel;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.apache.tomcat.jni.Time;
 
-import java.time.Clock;
+
 import java.time.LocalDateTime;
 import java.util.Date;
 
